b594db9a0d361b3dad18950e7a3d935f9519e0e5
Ibnul Hayat <53576655+ibnulhayat@users.noreply.github.com>
2022-08-22 20:17:17 +0600

    fist commit

---
+++ SubtractionFormAnArray.cs
@@ -0,0 +1,49 @@
+// Online C# Editor for free
+// Write, Edit and Run your C# code using C# Online Compiler
+
+using System;
+
+public class SubtractionFormAnArray{
+
+    public static void Main(string[] args){
+        var digits = new int[]{1, 2, 3};
+        var value = 2;
+        // we are print the input array and decremnts value
+        Print("Input",digits);
+        Console.Write("\t decrements = "+value+"\n");
+        // we are calling Decrease method for calculation
+        Decrease(digits, value);
+        // we are print the last output after decrease value
+        Print("Output",digits);
+    }
+    
+    // this method time complexcity is O(n)
+    // space complexcity is O(1)
+    // this pproach is Schoolbook subtraction
+    private static void Decrease(int[] digits, int decreaseValue){
+        var carry = 0;
+        for(var i = digits.Length-1; i >= 0; i--){
+            var deVal = (decreaseValue % 10) + carry;
+            if(digits[i] < deVal){
+                digits[i] = (digits[i] + 10) - deVal;
+                carry = 1;
+            }else{
+                digits[i] = digits[i] - deVal;
+                carry = 0;
+            }
+            decreaseValue /= 10;
+        }
+    }
+    
+    private static void Print(string type,int[] digits){
+        Console.Write(type+" = ");
+        for(int i=0; i< digits.Length; i++){
+            if(i == 0)
+                Console.Write("["+digits[i]+",");
+            else if(i == digits.Length-1)
+                Console.Write(digits[i]+"]");
+            else 
+                Console.Write(digits[i]+",");
+        }
+    }
+}
